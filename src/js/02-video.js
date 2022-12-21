b6952c7025697a86273bb6a97fa68ed7dce6a562
ilcommitfa <110323544+ilcommitfa@users.noreply.github.com>
2022-12-21 19:33:37 +0200

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+player.on(
+  'timeupdate',
+  throttle(({ seconds }) => {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  }, 1000)
+);
+
+const time = localStorage.getItem('videoplayer-current-time');
+
+player
+  .setCurrentTime(time || 0.1)
+  .then(function () {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        console.log('some other error occurred');
+
+        break;
+    }
+  });
